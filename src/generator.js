72fb88c6c345c0ad9f866bf57108cc82ff011fc7
Min ho Kim <minho42@gmail.com>
2021-07-01 00:29:51 +1000

    remove new promise resolve

--- src/generator.js
+++ src/generator.js
@@ -2,23 +2,16 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 const generator = async ({ url, selector }) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      if (!url || !selector) resolve({ error: "must provide url and selector" });
-      if (url.endsWith("/")) {
-        url.slice(0, -1);
-      }
+  if (!url || !selector) resolve({ error: "must provide url and selector" });
+  if (url.endsWith("/")) {
+    url.slice(0, -1);
+  }
 
-      const { data } = await axios.get(url);
-      const $ = cheerio.load(data);
-      const result = $(selector).text();
-      const uri = `${url}#:~:text=${result}`;
-      resolve(uri);
-    } catch (e) {
-      console.error(e);
-      reject(new Error(e));
-    }
-  });
+  const { data } = await axios.get(url);
+  const $ = cheerio.load(data);
+  const result = $(selector).text();
+  const uri = `${url}#:~:text=${result}`;
+  return uri;
 };
 
 module.exports = generator;
